d1f5a1ec63466269bbdf2a8cfd87bb2f2ac5c373
Asa Strong <asastrong19@gmail.com>
2018-04-22 22:34:55 -0700

    work on imagery stuff

--- imagery/satellite_data_analysis.py
+++ imagery/satellite_data_analysis.py
@@ -24,29 +24,81 @@ def authenticate(api_key, planet_data_url):
 
     return session
 
-def create_filter(geom):
+def create_filter(geom, gt_year, lt_year=None):
 
     # Setup Geometry Filter
-    filter = {
+    geom_filter = {
         "type": "GeometryFilter",
         "field_name": "geometry",
         "config": geom
     }
 
-    return filter
+    date_filter = {
+      "type": "DateRangeFilter",
+      "field_name": "acquired",
+      "config": {
+        "gt": f"{gt_year}-01-01T00:00:00Z",
+        }
+    }
+
+    and_filter = {
+    "type": "AndFilter",
+    "config": [geom_filter, date_filter]
+    }
+
+    return geom_filter, date_filter, and_filter
 
 def request_imagery_for_aoi(session, url, filter, item_types):
 
     request = {
         "item_types": item_types,
+        "interval": "year",
         "filter": filter
     }
 
     # Send the POST request to the API stats endpoint
-    res = session.post(url, json=request)
+    res = session.post(url, json=request).json()
 
     # Print response
-    p(res.json())
+    features = res['features']
+    print("total features: {}".format(len(features)))
+    Fids = []
+    data_dict = {}
+    for f in features:
+        data_dict[f['id']] = {}
+        data_dict[f['id']]['permissions'] = f['_permissions']
+        data_dict[f['id']]['assets'] = f['_links']['assets']
+        print("added {} to asset_dict".format(f['id']))
+
+    return data_dict
+
+def status_code(status):
+
+    if status == 202:
+        print("The request has been accepted and the activation will begin shortly.")
+    elif status == 204:
+        print("The asset is already active and no further action is needed.")
+    elif status == 401:
+        print("The user does not have permissions to download this file.")
+    else:
+        print("something went wrong")
+
+def activate(session, data_dict):
+
+    assets = {}
+
+    for image_id in data_dict.keys():
+        res = session.get(data_dict[image_id]['assets'])
+        asset = res.json()
+        activation_url = asset['visual']['_links']['activate']
+        assets[image_id] = {}
+        assets[image_id]['activation_url'] = activation_url
+
+    res_activate = session.get(assets[next(iter(assets))]['activation_url'])
+
+    # Print the response from the activation request
+    print(res.status_code)
+    status_code(res.status_code)
 
 if __name__ == '__main__':
 
@@ -60,8 +112,10 @@ if __name__ == '__main__':
 
     aoi = json.load(open('aoi.json'))
 
+    filter = create_filter(aoi, '2013')[2]
+
     session = authenticate(api_key, planet_data_url)
 
-    filter = create_filter(aoi)
+    data_dict = request_imagery_for_aoi(session, quick_url, filter, ["PSScene3Band", "REOrthoTile"])
 
-    request_imagery_for_aoi(session, quick_url, filter, ["PSScene3Band", "REOrthoTile"])
+    activate(session, data_dict)
