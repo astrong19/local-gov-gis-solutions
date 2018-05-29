3f5eb241af906a092142efb9c6cb295cfa5cb518
astrong19 <asastrong19@gmail.com>
2018-05-29 14:47:47 -0700

    get json from item helper

--- utilities/api_utility_functions.py
+++ utilities/api_utility_functions.py
@@ -5,6 +5,7 @@
 # Created on 5/21/18
 # https://community.esri.com/thread/199333-does-anyone-have-a-python-api-recipe-for-deleting-features-in-an-arcgis-online-hosted-feature-layer-im-specifically-looking-to-clear-out-all-the-features-in-several-layers
 
+import requests
 from arcgis.gis import GIS
 from getpass import getpass
 
@@ -31,16 +32,25 @@ class ApiUtilities(object):
 
         print("Deleted Features for {}".format(item.title))
 
-    def clone_item(self, item_id, copy_data):
+    def clone_item(self, item_id):
 
         item_to_clone = self.gis.content.get(item_id)
         data = [item_to_clone]
 
-        self.gis.content.clone_items(data, copy_data=copy_data, search_existing_items=False)
+        self.gis.content.clone_items(data, copy_data=False, search_existing_items=False)
         print("copied item {0} to {1}".format(item_to_clone.title, self.gis))
+
+    def get_json_from_item(self, item_id):
+
+        item = self.gis.content.get(item_id)
+        r = requests.get(item.url + '?f=pjson')
+        data = r.json()
+
+        print(data)
+        return data
 
 if __name__ == '__main__':
 
     item_id = '53244a1d3ca447d8a890ad6bbc43621b' #enter item id here
     api = ApiUtilities('astrong_pnw', getpass())
-    api.clone_item(item_id, False)
+    api.get_json_from_item(item_id)
